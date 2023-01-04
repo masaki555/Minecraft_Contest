8c06dd0f6610c4ec43ec0766f7812d825b991598
masaki <masaki.obana@oit.ac.jp>
2023-01-04 13:58:05 +0900

    testbot.cを配布用に修正

--- testbot.c
+++ testbot.c
@@ -2,42 +2,26 @@
 #include <unistd.h>
 #include "control.h"
 
-int printBinary(int value){
-    int binary = 0;
-    int base = 1;
-
-    while(value>0){
-        binary = binary + ( value % 2 ) * base;
-        value = value / 2;
-        base = base * 10;
-    }
-    return binary;
-}
-
 int main(char *argv){
-    int han = 0 , han2 = 0 , han3 = 0;
+    int han = 0 , han2 = 0 ;
+    long han3 = 0; 
     int flag=0;
     init();
-    //setTime();
+    setTime();
     exePython();
-    //setSurvival();
+    setSurvival();
     while(rk){
-        han = printBinary(detectZombie());
-        han2 = printBinary(detectZombie2());
-        han3 = printBinary(detectMobs(1));
-        /*
         han = detectZombie();
         han2 = detectZombie2();
         han3 = detectMobs(1);
-        */
+        
         printf("detectZombie1=\t%d\n" , han);
         printf("detectZombie2=\t%d\n" , han2);
         printf("detectMobs=\t%d\n" , han3);
         
         moveForward(1.5);
-        //cameraRight(2.0);
 
     }
-    //setCreative();
-    //setMorning();
+    setCreative();
+    setMorning();
 }
