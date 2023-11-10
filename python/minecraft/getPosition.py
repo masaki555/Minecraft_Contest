4f2e3217aaa5cfc440585ca572d388dd9a04edd4
maru <e1b21098@oit.ac.jp>
2023-11-10 16:23:47 +0900

    座標の取得(仮)

--- python/minecraft/getPosition.py
+++ python/minecraft/getPosition.py
@@ -2,18 +2,33 @@ import win32gui
 from PIL import ImageGrab, Image
 import pytesseract
 from tesseract_pack import data_here
+import time
+import pydirectinput
+
+##################################
+game_name = 'Minecraft Education'
+sleep_time = 0.05
+##################################
 
 def main():
+    mcapp = win32gui.FindWindow(None,game_name)
+    time.sleep(sleep_time)
+    win32gui.SetForegroundWindow(mcapp)         #ウィンドウの指定
+    time.sleep(sleep_time)
+    pydirectinput.keyDown('esc')
+    pydirectinput.keyUp('esc')
+    time.sleep(sleep_time)
+
     hwnd = win32gui.GetForegroundWindow()
-    rect = win32gui.GetWindowRect(hwnd)
-    print(rect) 
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    print(left, top, right, bottom)
+    cordinate = (left, top+130, right-780, bottom-845)
 
     TESSERACT_EXECUTABLE = data_here + "\\tesseract.exe"
     pytesseract.pytesseract.tesseract_cmd =  TESSERACT_EXECUTABLE
 
-    print(pytesseract.pytesseract.tesseract_cmd)
-
-    image = ImageGrab.grab(rect)
+    image = ImageGrab.grab(cordinate)
+    image.save('image.png')
     image.show()
     text = pytesseract.image_to_string(image)
 
