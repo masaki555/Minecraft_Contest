0774519d78409f83608eed3abb7ba454c9b485f8
morimoto <136801152+e1c22075@users.noreply.github.com>
2023-12-12 17:45:06 +0900

    getPosition.pyの修正

--- python/minecraft/getPosition.py
+++ python/minecraft/getPosition.py
@@ -3,54 +3,41 @@ import win32gui
 from PIL import ImageGrab, Image
 import numpy as np 
 
-
 script_dir = os.path.dirname(os.path.abspath(__file__)) 
 data_dir= script_dir + '/posdata/'
 
-
-
-
 def getPosition():
-    
 
     hwnd = win32gui.GetForegroundWindow()
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
     
     cordinate = (left+102, top+137, right-730, bottom-849)
-
     
-    
+    # スクショと文字認識
     image = ImageGrab.grab(cordinate)
-    
     
     imageGrayScale = np.array(image.convert('L'), 'f')
     imageGrayScale = (imageGrayScale < 150) * 255
     imageGrayScale = Image.fromarray(imageGrayScale)
     imageGrayScale = imageGrayScale.convert('RGBA')
-    
-    
-    
-    
 
     widthGrayScale = imageGrayScale.size[0]
-    
+    zahyouList=[]
     screenSize=0
     colorType=0
     flagWideth=0
-    line3=''
-    flagHeight=0
-    zahyouList=[]
-    positionValue=''
-    flag=0
     flagMinus=0
-    
-    
-    
+    flagLine=0
+    WHITE_PIXEL="(255, 255, 255, 255)"
+    BLACK_PIXEL="(0, 0, 0, 255)"
+    positionValue=''    
+    binary=''
+    line1=''
+    line2=''
+    line4=''
     
     while screenSize < widthGrayScale :
         imageCrop = imageGrayScale.crop((screenSize, 0, screenSize+12,14))
-        imageCrop.save("imageC.png")
-        
         
         widthC, heightC = imageCrop.size
         imageArray = np.empty((heightC, widthC), dtype = object)
@@ -60,146 +47,90 @@ def getPosition():
             # 順次、ピクセルの色の数値を代入している
                 imageArray[y][x] = imageCrop.getpixel((x, y))
                 
-        with open(data_dir+'imageColorValues.txt', mode='w') as imageToWidethValue:
+        with open(data_dir+'imageColorValues.txt', 'w', encoding='utf-8') as imageToHeightValue:
             for y in range(heightC):
                 for x in range(widthC):
                     
                     if(flagWideth==0):
-                        if(str(imageArray[y][x])==("(255, 255, 255, 255)")):
+                        if(str(imageArray[y][x])==WHITE_PIXEL):
                             colorType=0
-                        elif(str(imageArray[y][x])==("(0, 0, 0, 255)")):
+                        elif(str(imageArray[y][x])==BLACK_PIXEL):
                             colorType=1
                         else:
-                            colorType=2
+                            print("Error:")
                         
                         flagWideth=1
                     elif(flagWideth==1):
-                        if(str(imageArray[y][x])==("(255, 255, 255, 255)")and colorType==0):
-                            imageToWidethValue.write(str(0))
-                        elif(str(imageArray[y][x])==("(0, 0, 0, 255)")and colorType==1):
-                            imageToWidethValue.write(str(1))
+                        if(str(imageArray[y][x])==WHITE_PIXEL and colorType==0):
+                            binary+=str(0)
+                        elif(str(imageArray[y][x])==BLACK_PIXEL and colorType==1):
+                            binary+=str(1)
                         else:
-                            imageToWidethValue.write(str(2))
-                        
+                            print("Error:")
+                            
                         flagWideth=0
-                    
-                                        
-                
-                imageToWidethValue.write('\n')
-                
-                    
-                
-                    
                         
-                        
-            
-            
-                        
-                   
-        screenSize+=12
-       
-    
-        
-        with open(data_dir+'imageColorValues.txt', 'r', encoding='utf-8') as imageToWidethValue,  open(data_dir+'imageColorValues2.txt', 'w', encoding='utf-8') as imageToHeightValue:
-             
-                
-            while True:
-                if(flagHeight<7):
-                    line1 = imageToWidethValue.readline()
-                    line2 = imageToWidethValue.readline()
-                    if not line1 or not line2:
-                        break
+                if(flagLine==0):
+                    line1=binary
+                    flagLine=1
+                    
+                elif(flagLine==1):
+                    line2=binary
                     
                     if(line1==line2):
-                        line3+=(str(line1))
+                        imageToHeightValue.write(str(line1))
+                        
                     else:
-                        line3+=str(2)
+                        print("error")
                     
-                    
-                    flagHeight+=1
-                    
-                if(flagHeight>=7):
-                    line3 = line3.replace(',', '').replace('\n', '')
-                    imageToHeightValue.write(str(line3))
-                    imageToHeightValue.write('\n')
-                    line3=''
-                    flagHeight=0
-        
-        with open(data_dir+'imageColorValues2.txt', 'r', encoding='utf-8') as imageToHeightValue,open(data_dir+'imageALLColorValues.txt', 'r+', encoding='utf-8') as sampleData:           
-            while True: 
+                    flagLine=0
                 
-                line4 = imageToHeightValue.readline()
+                binary=''
                 
-                if not line4:
-                    
-                    
-                    flag+=1
+        with open(data_dir+'imageColorValues.txt', 'r', encoding='utf-8') as imageToHeightValue,open(data_dir+'imageALLColorValues.txt', 'r+', encoding='utf-8') as sampleData:           
+            
+            line4 = imageToHeightValue.readline()            
+            sampleData.seek(0)
+            
+            while True:
+                lineSampleBinary = sampleData.readline()
+                lineSampleValue = sampleData.readline()
+                lineSampleBinary = lineSampleBinary.strip()
+                
+                if not lineSampleBinary or not lineSampleValue:
                     break
                 
-                
-                sampleData.seek(0)
-                
-                while True:
-                    
-                    lineSampleBinary = sampleData.readline()
-                    lineSampleValue = sampleData.readline()
-                    
-                    
-                    
-                    
-                    if not lineSampleBinary or not lineSampleValue:
+                if(line4==lineSampleBinary):
+                    lineSampleValue = lineSampleValue.strip()
+            
+                    if(lineSampleValue=='-'):
+                        flagMinus=1
+                        positionValue=''
+                        sampleData.seek(0)
                         break
+                        
+                    elif(lineSampleValue==','):
+                            
+                        positionValue=int(positionValue)
+                        if(flagMinus==1):
+                            positionValue=positionValue*(-1)
+                            flagMinus=0
+                        
+                        zahyouList.append(positionValue)
+                        
+                        positionValue=''
+                        
+                    else:  
+                        positionValue += lineSampleValue 
                     
+        screenSize+=12
+        
+    positionValue=int(positionValue)
+    if(flagMinus==1):
+        positionValue=positionValue*(-1)
                     
-                    if(line4==lineSampleBinary):
-                        lineSampleValue = lineSampleValue.strip()
-                        
-                            
-                        if(lineSampleValue=='-'):
-                           
-                            flagMinus=1
-                            positionValue=''
-                            sampleData.seek(0)
-                            break
-                            
-                            
-                        elif(lineSampleValue==','):
-                            
-                            
-                            
-                            
-                                
-                            positionValue=int(positionValue)
-                            if(flagMinus==1):
-                                positionValue=positionValue*(-1)
-                                flagMinus=0
-                            
-                            
-                            zahyouList.append(positionValue)
-                            
-                            
-                            positionValue=''
-                            
-                            
-                        else:  
-                            positionValue += lineSampleValue 
-                        
-                        
-                        
-                if(flag==8):
-                    positionValue=int(positionValue)
-                    if(flagMinus==1):
-                        positionValue=positionValue*(-1)
-                        flagMinus=0
-                            
-                            
-                    zahyouList.append(positionValue)
-                   
-                            
-                    positionValue=''
-                            
+    zahyouList.append(positionValue)                   
 
-    
     
     return zahyouList
 
