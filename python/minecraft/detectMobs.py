4d7224e78dabcb5a59837c636da2c64643f6d921
tikutikuwa <otsuka63477@gmail.com>
2023-12-22 14:28:29 +0900

    complete detectMobs.py

--- python/minecraft/detectMobs.py
+++ python/minecraft/detectMobs.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import cv2
 import numpy as np
@@ -129,8 +128,8 @@ class DetectMobs:
 
     def write_result_to_text(self, bboxes, classes):
         with open(self.txt_path, encoding="UTF-8", mode="w") as f:
-            results_zommbie, results_sckelton = self.calc_location(bboxes, classes)
-            for result in results_zommbie:
+            results_zombie, results_sckelton = self.calc_location(bboxes, classes)
+            for result in results_zombie:
                 f.write(str(result) + " ")
             f.write("\n")
             for result in results_sckelton:
@@ -148,7 +147,7 @@ class DetectMobs:
         Returns:
             occupancy level in segments
         """
-        occupancy_grid_zommbie = np.zeros((640, 640))
+        occupancy_grid_zombie = np.zeros((640, 640))
         occupancy_grid_sckelton = np.zeros((640, 640))
 
         for bbox, cls in zip(bboxes, classes):
@@ -159,21 +158,21 @@ class DetectMobs:
             y_max = int(min(y_center + height / 2.0, 640))
 
             if cls == 0:
-                occupancy_grid_zommbie[y_min:y_max, x_min:x_max] = 1
+                occupancy_grid_zombie[y_min:y_max, x_min:x_max] = 1
             elif cls == 1:
                 occupancy_grid_sckelton[y_min:y_max, x_min:x_max] = 1
 
         segment_width = 640 // self.vertical_split_num
         segment_height = 640 // self.horizontal_split_num
 
-        occupancy_ratios_zommbie = np.zeros(
+        occupancy_ratios_zombie = np.zeros(
             self.horizontal_split_num * self.vertical_split_num
         )
-        occupancy_ratios_sckelton = np.zeros_like(occupancy_ratios_zommbie)
+        occupancy_ratios_sckelton = np.zeros_like(occupancy_ratios_zombie)
 
         for x in range(self.vertical_split_num):
             for y in range(self.horizontal_split_num):
-                segment_zommbie = occupancy_grid_zommbie[
+                segment_zombie = occupancy_grid_zombie[
                     y * segment_height : (y + 1) * segment_height,
                     x * segment_width : (x + 1) * segment_width,
                 ]
@@ -181,20 +180,20 @@ class DetectMobs:
                     y * segment_height : (y + 1) * segment_height,
                     x * segment_width : (x + 1) * segment_width,
                 ]
-                occupancy_ratio_zommbie = np.sum(segment_zommbie) / (
+                occupancy_ratio_zombie = np.sum(segment_zombie) / (
                     segment_width * segment_height
                 )
                 occupancy_ratio_sckelton = np.sum(segment_sckelton) / (
                     segment_width * segment_height
                 )
                 index = y * self.vertical_split_num + x
-                occupancy_ratios_zommbie[index] = occupancy_ratio_zommbie
+                occupancy_ratios_zombie[index] = occupancy_ratio_zombie
                 occupancy_ratios_sckelton[index] = occupancy_ratio_sckelton
 
-        occupancy_ratios_zommbie = (occupancy_ratios_zommbie * 10).astype(int)
+        occupancy_ratios_zombie = (occupancy_ratios_zombie * 10).astype(int)
         occupancy_ratios_sckelton = (occupancy_ratios_sckelton * 10).astype(int)
 
-        return occupancy_ratios_zommbie, occupancy_ratios_sckelton
+        return occupancy_ratios_zombie, occupancy_ratios_sckelton
 
     def detect(self):
         """this is main process in this class"""
@@ -215,26 +214,28 @@ class DetectMobs:
         self.write_result_to_text(bboxes, classes)
 
     def main(self):
-        # while True:
-        self.detect()
+        while True:
+            self.detect()
 
-        if self.is_save_result:
-            self.show_result_img()
+        # debug show
+        #if self.is_save_result:
+        #    self.show_result_img()
 
 
 if __name__ == "__main__":
+    with open('yoloFiles/detectMobs_config.txt', 'r') as file:
+        v = int(file.readline())
+        h = int(file.readline())
     detect = DetectMobs(
+        # debug input
         img_path=os.path.join(base_dir, "yoloFiles/input.png"),
         capture_path="yoloFiles/capture.png",
         txt_path="yoloFiles/labels/capture.txt",
         input_img_size=640,
         is_save_result=False,
-        vertical_split_num=5,
-        horizontal_split_num=5,
+        vertical_split_num=v,
+        horizontal_split_num=h,
         confidence=0.6,
     )
-    start_time = time.perf_counter()
-    detect.main()
-    end_time = time.perf_counter()
 
-    print(f"Execution time: {end_time - start_time} seconds")
+    detect.main()
