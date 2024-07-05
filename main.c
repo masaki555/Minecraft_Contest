64bb992eaed318f8aa67ebbf3a1b56bafa9cd712
72q84g <135799349 +72q84g@users.noreply.github.com>
2024-07-05 17:49:31 +0900

    first

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "control.h"
+
+int main(char *argv){
+    init();
+    setTime();
+    exePython();
+    setSurvival();
+    while(rk){
+        
+    }
+    setCreative();
+    setMorning();
+}
