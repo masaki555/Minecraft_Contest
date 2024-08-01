ed1d26fa824e8eff529db17b8e4e22216e5009bc
72q84g <135799349 +72q84g@users.noreply.github.com>
2024-08-02 00:38:50 +0900

    equipment

--- python/minecraft/equipmentDev.py
+++ python/minecraft/equipmentDev.py
@@ -2,7 +2,6 @@ import sys
 import time
 sys.path.append('.')
 from python.minecraft import pushKey
-import keyboard
 
 def run(command):
     time.sleep(0.5)
@@ -10,18 +9,12 @@ def run(command):
         pushKey.push(i)
 
 def equipment():
+    time.sleep(1)
     pushKey.push("2")
     pushKey.push("e")
     pushKey.push("3")
     pushKey.push("e")
     pushKey.push("1")
-
-    pushKey.push('enter')
-    run("/tp ")
-    keyboard.send('@')
-    time.sleep(0.5)
-    run("s 0 4 0")
-    pushKey.push('enter')
 
 if __name__ == '__main__':
     equipment()
