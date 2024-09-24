2e904c0e9d17afc2f69b4585b4967fa3d38179da
72q84g <135799349 +72q84g@users.noreply.github.com>
2024-09-24 17:57:04 +0900

    update

--- control.c
+++ control.c
@@ -22,7 +22,7 @@ pid_t Camera_pid;
 int rk = 1;
 
 void equipmentDev(void) {
-    char com[128] = "python/python.exe python/minecraft/equipmentDev.py";
+    char com[128] = "python/python.exe python/minecraft/equipment.py";
     int f = system(com);
     if (f != 0 && WEXITSTATUS(f) != 0) {
         printf("error:equipmentDev\n");
--- testbot.c
+++ testbot.c
@@ -5,12 +5,10 @@
 int main(int argc, char *argv[]){
     init();          //Minecraftのゲームコントロール関数．ウィンドウサイズを設定する等を行う．
     exePython();     //画像処理プログラムを実行する関数．
-    setSurvival();   //サバイバルモードにする．
     while(rk){       //無限loopする．rkはF12キーを押すと0となり，プログラムが停止します．
-        /*ここからBotプログラムを書く*/
-        printf("ゾンビ:%d\n",detectZombie3());
-
+        /*ここからBotプログラム を書く*/
+        sleep(15);
+        equipmentDev();
         /*ここまでBotプログラムを書く*/
-        sleep(1);
     }
 }
